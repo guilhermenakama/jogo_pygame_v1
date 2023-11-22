d9632c98d02d6e5f77f5ab79b65c56d17905ca64
lukfernandes <luccademagalhaes@gmail.com>
2023-11-22 15:48:46 -0300

    definição background (precisa trabalho)

--- assets.py
+++ assets.py
@@ -14,7 +14,10 @@ def load_assets():
     assets[PLAYER_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'penguin.png')).convert_alpha()
     assets[PLAYER_IMG] = pygame.transform.scale(assets[PLAYER_IMG], (PLAYER_WIDTH, PLAYER_HEIGHT))
     assets[BIRD_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'bird.png')).convert()
-    assets[BACKGROUND_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'background.jpeg')).convert()
+    assets[BIRD_IMG] = pygame.transform.scale(assets[BIRD_IMG], (BIRD_WIDTH, BIRD_HEIGHT))
+    assets[BACKGROUND_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'background_atualizado2.jpg')).convert()
+    assets[BACKGROUND_IMG] = pygame.transform.scale(assets[BACKGROUND_IMG], (WIDTH, HEIGHT))
+    assets[BACKGROUND_IMG] = BACKGROUND_IMG.get_rect()
 
     # Carrega os sons do jogo
     return assets
