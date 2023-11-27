ecbf0a32179406a55de80e33b64138f6e1fa16e7
morgana0carvalho <otomemari@gmail.com]>
2023-11-27 20:42:24 -0300

    tentativa

--- assets.py
+++ assets.py
@@ -4,8 +4,8 @@ from config import *
 
 BACKGROUND_INIT_IMG = 'background_init'
 BACKGROUND_IMG = 'background'
-PLAYER_IMG = 'player_img'
-PLAYER2_IMG='rena_img'
+PLAYER_IMG = 'player0_img'
+PLAYER2_IMG='player1_img'
 BIRD_IMG = 'snowball_img'
 
 def load_assets():
